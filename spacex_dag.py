c31d45d5c4081ec6d13861ecb78dc139c56ba8ed
root <root@airflow.europe-west4-a.c.rt-2021-02-28-12-46-08-09qni.internal>
2021-02-28 17:34:11 +0000

    true version

--- spacex_dag.py
+++ spacex_dag.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2018, 1, 1),
+    "start_date": datetime(2005, 1, 1),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -16,17 +16,21 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-    dag=dag
-)
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
 
-t1 >> t2
+for r in ['all', 'falcon', 'falcon9', 'falconheavy']:
+    t1 = BashOperator(
+        task_id="get_data_" + r , 
+        bash_command="python3 /root/airflow/dags/module-3-airflow/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data/ -r {{ params.rocket }} ", 
+        params={"rocket": r},
+        dag=dag
+    )
+    t2 = BashOperator(
+        task_id="print_data_" + r, 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+        params={"rocket": r}, # falcon1/falcon9/falconheavy
+        dag=dag
+    )
+    t1 >> t2
+
+
